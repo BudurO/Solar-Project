c9038c4d403ec2c89f3df8a9ad0519be4813ff90
BudurO <buduralthubyani@gmail.com>
2023-09-07 14:17:05 +0300

    add Routes in App file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import SingnUp from './Pages/SignUp'
 import Navbar from './Components/Navbar'
 import './App.css'
 import Footer from './Components/Footer'
+import { Routes, Route } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-   <Navbar/>
+  <Navbar/>
 
-   <SingnUp/>
+  <Routes>
+    <Route path="/" element={ "" }/>
+    <Route path="/" element={ <SingnUp/> }/>
+  </Routes>
   
   <Footer/>
     </>
