939ec1ef75211ff205c062b745783da9e11577ea
amzan1 <amzanabdullah97@gmail.com>
2023-09-10 15:31:35 +0300

    done user dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import './App.css'
 import Footer from './Components/Footer'
 import { Routes, Route } from "react-router-dom"
 import RentingForm from './Pages/RentingForm'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
 
   return (
     <>
-  <Navbar/>
+    
+    <Navbar/>
+
 
   <Routes>
     <Route path="/" element={ "" }/>
     <Route path="/SingnUp" element={ <SingnUp/> }/>
     <Route path='/rent' element={<RentingForm/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}>
+      {/* <Route path='/:id' element={<Dashboard/>}/> */}
+    </Route>
   </Routes>
 
   <Footer/>
