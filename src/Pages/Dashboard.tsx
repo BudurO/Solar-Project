17b5d56e09bbbaec98ff844f568c8ab3ee03c3dc
amzan1 <amzanabdullah97@gmail.com>
2023-09-10 16:30:08 +0300

    done user dash responsive

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -87,16 +87,16 @@ function Dashboard() {
                         </a>
                     </nav>
                 </div>
-                <div className="flex-1 p-4 w-full md:w-1/2">
-                    <div className=" bg-white pb-4 shadow rounded-lg">
-                        <table className="w-full table-auto text-sm mb-4 ">
-                            <thead className='bg-yellow-200 rounded-t-lg'>
-                                <tr className="text-sm leading-normal">
-                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">ID</th>
-                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">House address</th>
-                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Status</th>
-                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Phone</th>
-                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">Area of roof</th>
+                <div className="flex-1 p-4 lg:w-full md:w-1/2 ">
+                    <div className=" bg-white pb-4 shadow rounded-lg lg:w-full md:w-full w-auto">
+                        <table className="lg:w-full table-auto text-sm mb-4 md:w-1/2 w-[10]">
+                            <thead className='bg-yellow-200 rounded-t-lg lg:w-full md:w-full w-auto'>
+                                <tr className="text-sm leading-normal w-[20vw] lg:w-full md:w-1/2">
+                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase  text-[.4rem] text-grey-light border-b border-grey-light lg:text-sm md:text-sm">ID</th>
+                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-[.4rem] text-grey-light border-b border-grey-light lg:text-sm md:text-sm ">House address</th>
+                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-[.4rem] text-grey-light border-b border-grey-light lg:text-sm md:text-sm ">Status</th>
+                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-[.4rem] text-grey-light border-b border-grey-light lg:text-sm md:text-sm ">Phone</th>
+                                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-[.4rem] text-grey-light border-b border-grey-light lg:text-sm md:text-sm ">Area of roof</th>
                                     <th></th>
                                 </tr>
                             </thead>
@@ -105,14 +105,14 @@ function Dashboard() {
                                 {Info.map((item)=>{
                                     return(
                                         <tr className="hover:bg-grey-lighter" key={item.id}>
-                                            <td className="py-2 px-4 border-b border-grey-light border border-dashed  rounded-full h-10 w-10">#{cunt++}</td>
-                                            <td className="py-2 px-4 border-b border-grey-light border-r">{item.Locaiton.placeName}</td>
-                                            <td className="py-2 px-4 border-b border-grey-light border-r">{item.State}</td>
-                                            <td className="py-2 px-4 border-b border-grey-light border-r">{item.Number}</td>
-                                            <td className="py-2 px-4 border-b border-grey-light border-r">{item.Area}</td>
+                                            <td className="py-2 px-4 border-b border-grey-light border border-dashed  rounded-full h-10 w-10 text-[.4rem] lg:text-sm md:text-sm">#{cunt++}</td>
+                                            <td className="py-2 px-4 border-b border-grey-light border-r text-[.4rem] lg:text-sm md:text-sm">{item.Locaiton.placeName}</td>
+                                            <td className="py-2 px-4 border-b border-grey-light border-r text-[.4rem] lg:text-sm md:text-sm">{item.State}</td>
+                                            <td className="py-2 px-4 border-b border-grey-light border-r text-[.4rem] lg:text-sm md:text-sm">{item.Number}</td>
+                                            <td className="py-2 px-4 border-b border-grey-light border-r text-[.4rem] lg:text-sm md:text-sm">{item.Area}</td>
                                             <td className="py-2 px-4 border-b border-grey-light">
-                                                <button onClick={()=> deleteRent(String(item.id))} className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-red-600 dark:hover:bg-red-700">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
+                                                <button onClick={()=> deleteRent(String(item.id))} className="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-[.4rem] px-3 py-2 text-center inline-flex items-center  dark:bg-red-600 dark:hover:bg-red-700">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className=" w-4 h-4 " viewBox="0 0 16 16"> <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/> <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/> </svg>
                                                 </button>
                                             </td>
                                         </tr>
